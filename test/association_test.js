325977946333bbc74eaccb5815e434e77ae48c59
Dan Molitor <danmolitor91@gmail.com>
2016-12-19 14:30:24 -0500

    (feat) created tests for utilizing ref associations and populating

---
+++ test/association_test.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/users');
+const Comment = require('../src/comments');
+const BlogPost = require('../src/blogPosts');
+
+describe('Associations', () => {
+  let joe, blogPost, comment;
+  beforeEach( (done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is!' });
+    comment = new Comment ({ content: 'Congrats!'});
+
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+    Promise.all([ joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
+    });
+
+    it('saves a relation between a user and a blogpost', (done)=> {
+      User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
+        .then((user) => {
+          assert(user.blogPosts[0].title === 'JS is Great');
+          done();
+        });
+  });
+
+    it('saves a full relation graph', (done) => {
+      User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats!');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      });
+    });
+});
